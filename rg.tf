6e27b8a4d69e16b71575d6ba37d7f215c85a4635
tejas <tejas.shah@vinsys.com>
2026-03-12 11:28:44 +0000

    added new resource corrected

--- rg.tf
+++ rg.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example2" {
   name     = "example-hcp-tejas2"
   location = "West Europe"
 }
